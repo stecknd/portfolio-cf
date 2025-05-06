3d38e01e145aad3d1fab4fb57babddefa54c9230
David Steckner <davidsteckner@gmail.com>
2025-05-05 22:26:11 -0400

    fixed deployment?v2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
-<h2>Fixed Depolyment?</h2>
+<h2>Fixed Depolyment?v2</h2>
 <h2>By: David</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
